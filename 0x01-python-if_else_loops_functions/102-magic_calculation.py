92f4fb3e783e17edd0ad2f793c9b0a82b1acaa38
sungnga <708@holbertonschool.com>
2019-05-01 06:14:12 +0000

    Remove import

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-import dis
-dis.dis(magic_calculation)
-
-
 def magic_calculation(a, b, c):
     if a < b:
         return c
     elif c > b:
         return a + b
-    return a + b + c
+    return (a + b) - c
