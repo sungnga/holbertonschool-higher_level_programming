a73678a4c4b7b2499fb4bc3767782d40934512a7
sungnga <708@holbertonschool.com>
2019-05-03 03:58:16 +0000

    Connect if statments

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,11 +10,11 @@ if __name__ == "__main__":
     b = int(sys.argv[3])
     if sys.argv[2] == "+":
             print("{} + {} = {}".format(a, b, add(a, b)))
-    if sys.argv[2] == "-":
+    elif sys.argv[2] == "-":
             print("{} - {} = {}".format(a, b, sub(a, b)))
-    if sys.argv[2] == "*":
+    elif sys.argv[2] == "*":
             print("{} * {} = {}".format(a, b, mul(a, b)))
-    if sys.argv[2] == "/":
+    elif sys.argv[2] == "/":
             print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
