639b978c6cfdeb569f3cc36bf5b9fb32d4bcdf65
sungnga <708@holbertonschool.com>
2019-05-07 23:51:48 +0000

    Add a space

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,7 +4,7 @@ def print_reversed_list_integer(my_list=[]):
         for i in my_list[::-1]:
             print("{:d}".format(i))
 
-#alternative solution
+# alternative solution
 #    if my_list:
 #        my_list.reverse()
 #        for i in my_list:
