1783931ea65431f6257165558542937460f98727
sungnga <708@holbertonschool.com>
2019-05-08 05:48:35 +0000

    Delete tuple and write alternative solution

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple = (len(sentence), sentence[0])
-    if not sentence:
-        tuple[1] = None
-        return (tuple[0], tuple[1])
-    return (tuple[0], tuple[1])
+    if sentence:
+        return len(sentence), sentence[0]
+    return len(sentence), None
