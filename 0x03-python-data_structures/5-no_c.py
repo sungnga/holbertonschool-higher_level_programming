7fa555249fc3e73b0a4a1ba33f674b5f8e7964ba
sungnga <708@holbertonschool.com>
2019-05-08 01:12:57 +0000

    Change indentation

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
-# Write a function that removes all characters c and C from a string
-
-
 def no_c(my_string):
     new_str = ''
     if my_string:
         for i in my_string:
             if i != 'C' and i != 'c':
                 new_str += i
-        return new_str
+    return new_str
