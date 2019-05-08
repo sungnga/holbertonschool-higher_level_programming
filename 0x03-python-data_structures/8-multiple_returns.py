e6076a7ca943e6b998f8b63eaa5ec597a3ba970f
sungnga <708@holbertonschool.com>
2019-05-08 05:40:45 +0000

    Add a return statement if sentence is empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,5 @@ def multiple_returns(sentence):
     tuple = (len(sentence), sentence[0])
     if not sentence:
         sentence[0] = None
+        return (tuple[0], tuple[1])
     return (tuple[0], tuple[1])
