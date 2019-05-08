91e2058294a4c18ffd7c073a1352c78682c86356
sungnga <708@holbertonschool.com>
2019-05-08 05:43:22 +0000

    Change sentence to tuple in if statement

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     tuple = (len(sentence), sentence[0])
     if not sentence:
-        sentence[0] = None
+        tuple[1] = None
         return (tuple[0], tuple[1])
     return (tuple[0], tuple[1])
