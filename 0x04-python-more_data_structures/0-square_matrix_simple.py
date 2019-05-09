a038d069da4c1ba8d50f7cd6b408777cad238c9f
sungnga <708@holbertonschool.com>
2019-05-09 21:50:21 +0000

    Remove new_list variable

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
     for row in matrix:
         for elem in row:
-            new_matrix = [[elem**2 for elem in row] for row in matrix]
-        return new_matrix
+            return [[elem**2 for elem in row] for row in matrix]
+        return matrix
