c6e31399b8d6c020d685873fbe011ced5983733e
sungnga <708@holbertonschool.com>
2019-05-10 04:43:22 +0000

    Change from print to return

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    result = map(lambda x: x * number, my_list)
-    print(list(result))
+    return list(map(lambda x: x * number, my_list))
