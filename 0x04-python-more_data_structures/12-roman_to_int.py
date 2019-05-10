45377a2ad7121c754d01c4c5a1dea95869c902c4
sungnga <708@holbertonschool.com>
2019-05-10 21:20:19 +0000

    Initial commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     a_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if roman_string:
-        {key: int(value) for key, value in roman_string.iteritems()}
-        return a_dict
-    return 0
+    sum = 0
+    if roman_string and type(roman_string) is str:
+        for c in range(len(roman_string)):
+            if c + 1 >= len(roman_string):
+                sum += a_dict[roman_string[c]]
+                break
+            if a_dict[roman_string[c + 1]] <= a_dict[roman_string[c]]:
+                sum += a_dict[roman_string[c]]
+            else:
+                sum -= a_dict[roman_string[c]]
+        return sum
+    else:
+        return 0
