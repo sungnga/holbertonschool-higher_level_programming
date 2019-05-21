9f448783e5a8b059c5c3bff6f78c8862335f264a
sungnga <708@holbertonschool.com>
2019-05-21 21:29:51 +0000

    Initial commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """A square class"""
+    __size = None
+
+    def __init__(self, size):
+        """Initialize a square class"""
+        self.__size = size
