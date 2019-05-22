5f93fa0443411e094d5b126b14f5595177e8b5ee
sungnga <708@holbertonschool.com>
2019-05-22 22:12:24 +0000

    add a string variable to print string

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-class Square:
+class Square():
     """A square class"""
 
     def __init__(self, size=0, position=(0, 0)):
@@ -42,6 +42,20 @@ class Square:
         if type(value[1]) != int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integer")
         self.__position = value
+
+    def __str__(self):
+        """Prints string with the same behavior as my_print method"""
+        new_str = ""
+        if self.__size == 0:
+            return new_str
+
+        for j in range(self.__position[1]):
+            new_str += '\n'
+        for i in range(self.__size):
+            new_str += ' ' * self.__position[0]
+            new_str += '#' * self.__size
+            new_str += '\n'
+        return new_str[:-1]
 
     def my_print(self):
         """Prints in stdout the square with the character #"""
