303de494b4876a8f0643164ce61ee96287e3c76c
sungnga <708@holbertonschool.com>
2019-06-06 05:06:14 +0000

    Initial commit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+A module for BaseGeometry class.
+"""
+
+
+class BaseGeometry():
+    """A BaseGeometry class."""
+
+    def area(self):
+        """A method that raises an exception."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """A method that validates value."""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """A rectangle class."""
+
+    def __init__(self, width, height):
+        """Initialize the rectangle class."""
+        self.__width = width
+        self.__height = height
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
+
+    def area(self):
+        """A method that returns the rectangle area."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """A method that creates a string object from a given object"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
