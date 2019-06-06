ead9f2cdab892e73ca6e412ef4b522aeeff745ee
sungnga <708@holbertonschool.com>
2019-06-06 01:26:43 +0000

    Initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+A module for BaseGeometry class.
+"""
+
+
+class BaseGeometry():
+    """A BaseGeometry class."""
+
+    def area(self):
+        """A method that raises an exception."""
+        raise Exception("area() is not implemented")
