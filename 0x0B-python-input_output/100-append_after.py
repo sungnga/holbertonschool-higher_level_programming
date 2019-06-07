3248e46391dbf8eccf28d4df88d2c7a3f170cadb
sungnga <708@holbertonschool.com>
2019-06-07 07:30:18 +0000

    add indentation

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,8 +4,8 @@
 
 
 def append_after(filename="", search_string="", new_string="")
-    """Inserts a line of text to a file, after each line containing a specific
-    string."""
+    """Inserts a line of text to a file, after each line containing a
+    specific string."""
     with open(filename, encoding='utf-8') as f:
         for i in f:
             if search_string == i:
