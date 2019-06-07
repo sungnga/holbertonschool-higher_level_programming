c9db87fa527729f2d52c799ec9357d121e9e1cc3
sungnga <708@holbertonschool.com>
2019-06-07 08:07:00 +0000

    Add import parent class module

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,20 +4,7 @@
 A module for BaseGeometry class.
 """
 
-
-class BaseGeometry():
-    """A BaseGeometry class."""
-
-    def area(self):
-        """A method that raises an exception."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """A method that validates value."""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
