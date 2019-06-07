4cfe53502e0af2cb881cdeeb2ac04f0b4be6bcb6
sungnga <708@holbertonschool.com>
2019-06-07 07:19:11 +0000

    Add underscore to __dict__

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -27,4 +27,4 @@ class Student:
         """Replaces all attributes of the Student instance."""
         for i in self.__dict__:
             if i in json:
-                self.__dict_[i] = json[i]
+                self.__dict__[i] = json[i]
