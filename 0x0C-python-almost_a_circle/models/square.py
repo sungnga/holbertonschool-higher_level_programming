5c23ba1d08f58c610c5af23d55813f67696d87b5
sungnga <708@holbertonschool.com>
2019-06-12 17:48:16 +0000

    Delete setting size to private

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,8 +10,8 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize square class"""
-        super().__init__(size, size, x, y, id)
         self.size = size
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
@@ -25,7 +25,6 @@ class Square(Rectangle):
             raise TypeError("width must be an integer")
         if size <= 0:
             raise ValueError("width must be > 0")
-        self.__size = size
         self.__width = size
         self.__height = size
 
