efa761b5ea4a55d926555aa8db861149fb8797ed
sungnga <708@holbertonschool.com>
2019-07-16 04:47:53 +0000

    Add () around the compute function

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,6 +1,6 @@
 -- A script that computes the score average of all records in the table
 -- second_table of the database hbtn_0c_0 in your MySQL server
-ALTER TABLE second_table ADD average AS AVG(score);
+ALTER TABLE second_table ADD average AS (AVG(score));
 
 SELECT average
 FROM second_table;
