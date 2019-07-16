fb6e6baf29c058d6528db51e8de9fd119af3915c
sungnga <708@holbertonschool.com>
2019-07-16 22:18:15 +0000

    Add ORDER BY to sort list by cities id

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- A script that lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON cities.id=states.id;
+LEFT JOIN states ON cities.state_id=states.id
+ORDER BY cities.id ASC;
