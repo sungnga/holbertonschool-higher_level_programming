29f9afcfb87260d2619458d28c024115d6f19dd2
sungnga <708@holbertonschool.com>
2019-07-16 19:37:40 +0000

    Change id to state_id for foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,5 +3,5 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
        id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
-       state_id INT NOT NULL FOREIGN KEY (id) REFERENCES states(id),
+       state_id INT NOT NULL FOREIGN KEY (state_id) REFERENCES states(id),
        name VARCHAR(256) NOT NULL);
