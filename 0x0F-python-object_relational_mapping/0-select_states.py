a14c4cfcf1fb22dfb53ead88220a3c5f42c123ca
sungnga <708@holbertonschool.com>
2019-08-10 16:02:10 +0000

    Initial commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A script that lists all states from the database hbtn_0e_0_usa.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost", port=3306, user="root",
+                           passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A script that takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost", port=3306, user="root",
+                           passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
