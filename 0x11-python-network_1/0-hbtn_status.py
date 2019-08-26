c54318f2d60ecd305aafbcb22be3a5cc9ab34a0b
sungnga <708@holbertonschool.com>
2019-08-26 21:07:13 +0000

    Initial commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A Python script that fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print("Body response:\n\
+    \t- type: {}\n\
+    \t- content: {}\n\
+    \t- utf8 content: {}".format(type(html), html, html.decode('utf8')))
