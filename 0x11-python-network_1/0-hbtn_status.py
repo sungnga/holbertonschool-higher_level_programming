4c3f8d7e7acefed732f2c52610483ba6a58e7a93
sungnga <708@holbertonschool.com>
2019-08-26 21:14:25 +0000

    Add condition so that code doesn't execute when imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,10 @@
 """A Python script that fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
-
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("Body response:\n\
-    \t- type: {}\n\
-    \t- content: {}\n\
-    \t- utf8 content: {}".format(type(html), html, html.decode('utf8')))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:\n\
+        - type: {}\n\
+        - content: {}\n\
+        - utf8 content: {}".format(type(html), html, html.decode('utf8')))
