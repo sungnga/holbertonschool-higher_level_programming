d2dd02b1a4f1c57820923f4d2caa2ad102958a6c
sungnga <708@holbertonschool.com>
2019-09-17 17:51:34 +0000

    Initial commit

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const lineOne = 'C is fun';
+const lineTwo = 'Python is cool';
+const lineThree = 'Javascript is amazing';
+console.log(lineOne);
+console.log(lineTwo);
+console.log(lineThree);
