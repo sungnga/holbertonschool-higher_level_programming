0cb5f226b7710549a2acb8ff72469b1e1d71050e
sungnga <708@holbertonschool.com>
2019-09-17 20:30:19 +0000

    Remove for loop

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
 // A script that prints a message depending of the number of arguments passed
 
-'use strict';
-
-// var myArgs = process.argv.slice(2);
-for (let j = 1; j < process.argv.length; j++) {
-  if (j < 2) {
-    console.log('No argument');
-  } else if (j === 2) {
-    console.log('Argument found');
-  } else {
-    console.log('Arguments found');
-  }
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
