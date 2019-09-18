4de95ff5bbff1324f77f4f89c3bcf65d24ee9f18
sungnga <708@holbertonschool.com>
2019-09-18 02:58:06 +0000

    Use for loop for iteration

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 // A script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
 
 const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-lines.forEach(function (line) {
-  console.log(line);
-});
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
