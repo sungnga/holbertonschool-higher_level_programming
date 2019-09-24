c9a1b0df40a45b0c64b4927330b610db9696f09f
sungnga <708@holbertonschool.com>
2019-09-24 22:57:23 +0000

    Remove utf-8 encoding for get request

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,12 +7,12 @@ const fs = require('fs');
 const url = process.argv[2];
 const filepath = process.argv[3];
 
-request({ url: url, json: true }, 'utf-8', (err, res) => {
-  const content = res.body;
+request({ url: url, json: true }, (err, res) => {
   if (err) {
     console.log(err);
   } else {
-    fs.writeFile(filepath, content, 'utf-8', (err) => {
+    // const content = res.body;
+    fs.writeFile(filepath, res.body, 'utf-8', (err) => {
       if (err) {
         console.log(err);
       }
