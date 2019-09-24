1000fdfcf890612f7a36b7034ff8a40b02f43577
sungnga <708@holbertonschool.com>
2019-09-24 22:13:19 +0000

    Initial commit

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// A script that gets the contents of a webpage and stores it in a file
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filepath = process.argv[3];
+
+request({ url: url, json: true }, (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const content = res.body;
+    fs.writeFile(filepath, content, 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
