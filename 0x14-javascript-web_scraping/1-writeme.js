190f1d43873742c1e61707b402cd0ad63f2122fa
sungnga <708@holbertonschool.com>
2019-09-24 20:23:39 +0000

    Update comment for task

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// Write a script that reads and prints the content of a file
+// Write a script that writes a string to a file
 
 const fs = require('fs');
 const filepath = process.argv[2];
