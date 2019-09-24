98527b37b29d5a84b1f60f488d7aa78da8bb042a
sungnga <708@holbertonschool.com>
2019-09-24 23:57:44 +0000

    Change conditional statement

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,17 +5,16 @@ const request = require('request');
 const fs = require('fs');
 
 const url = process.argv[2];
-const filepath = process.argv[3];
+// const filepath = process.argv[3];
 
-request(url, (err, res, body) => {
+request({ url: url, json: true }, (err, res) => {
+  const content = res.body;
   if (err) {
     console.log(err);
-  } else {
-    const content = body.body;
-    fs.writeFile(filepath, content, 'utf-8', (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-    });
   }
+  fs.writeFile(process.argv[3], content, 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 });
