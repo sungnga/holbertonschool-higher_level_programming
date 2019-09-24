01eb9045479e0843991f38c7dcacc09c0b43bf92
sungnga <708@holbertonschool.com>
2019-09-24 01:49:01 +0000

    Update comment for the task

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-// Write a class Rectangle that defines a rectangle
+// Write a class Rectangle that defines a rectangle and creates an instance
+// method called print()
 
 class Rectangle {
   constructor (w, h) {
