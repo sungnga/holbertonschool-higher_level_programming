90860eaf946986e30af0f423da836f4bc3c1f0f7
sungnga <708@holbertonschool.com>
2019-09-24 07:30:12 +0000

    Initial commit

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Write a script that concats 2 files
+
+const fs = require(fs);
+
+const a = process.argv[2];
+const b = process.argv[3];
+const c = process.argv[4];
+
+const fileA = fs.readFileSync(a);
+const fileB = fs.readFileSync(b);
+const fileC = fileA + fileB;
